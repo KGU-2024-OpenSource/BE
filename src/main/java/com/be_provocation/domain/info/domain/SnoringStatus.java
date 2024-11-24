d3e28481050b31794f6e13a099d2b30b0837ee7d
gyuseon <mn5592@naver.com>
2024-11-24 15:23:25 +0900

    Feat: 코골이여부 Enum 생성 (#13)

---
+++ src/main/java/com/be_provocation/domain/info/domain/SnoringStatus.java
@@ -0,0 +1,28 @@
+package com.be_provocation.domain.info.domain;
+
+import com.be_provocation.global.exception.CheckmateException;
+import com.be_provocation.global.exception.ErrorCode;
+import java.util.Arrays;
+
+public enum SnoringStatus {
+    YES("O"),
+    NO("X"),
+    SOMETIMES("피곤하면 가끔");
+
+    private final String displayName;
+
+    SnoringStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public static SnoringStatus fromDisplayName(String displayName) {
+        return Arrays.stream(SnoringStatus.values())
+                .filter(status -> status.getDisplayName().equals(displayName))
+                .findFirst()
+                .orElseThrow(() -> CheckmateException.from(ErrorCode.ENUM_CONVERSION_ERROR));
+    }
+}
