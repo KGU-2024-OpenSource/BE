0570c3c5beaa9ab4df48013d175bcf3453ff105f
mingmingmon <mingmingmon@naver.com>
2024-11-27 14:49:40 +0900

    Fix: 프로퍼티 config 설정 (#19)

---
+++ src/main/java/com/be_provocation/global/config/PropertyConfig.java
@@ -0,0 +1,12 @@
+package com.be_provocation.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:.env.docker")
+})
+public class PropertyConfig {
+}
--- src/test/java/com/be_provocation/BeProvocationApplicationTests.java
+++ src/test/java/com/be_provocation/BeProvocationApplicationTests.java
@@ -2,8 +2,10 @@ package com.be_provocation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BeProvocationApplicationTests {
 
 	@Test
