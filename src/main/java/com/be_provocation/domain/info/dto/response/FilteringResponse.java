a573bb93ab549d1e5ceed6f69a598888bdbdde90
gyuseon <mn5592@naver.com>
2024-11-28 16:34:20 +0900

    Refactor: 반환 값에 MBTI 추가 (#21)

--- src/main/java/com/be_provocation/domain/info/dto/response/FilteringResponse.java
+++ src/main/java/com/be_provocation/domain/info/dto/response/FilteringResponse.java
@@ -1,9 +1,12 @@
 package com.be_provocation.domain.info.dto.response;
 
+import com.be_provocation.domain.info.domain.MBTI;
+
 public record FilteringResponse
         (Long myInfoId,
          String nickname,
          String profileImageUrl,
+         MBTI mbti,
          Integer studentId,
          Integer birthYear,
          String department,
